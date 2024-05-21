5ae69ac9feff6df01aeb5ce2977988eeee7d46a0
TaiyoYamashita <sunlight360@icloud.com>
2024-05-21 14:46:09 +0900

    postsマイグレーション

---
+++ database/migrations/2024_05_21_052306_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('title');
+            $table->string('sentences');
+            $table->string('memorandum');
+            $table->integer('private_or_public');
+            $table->timestamp('saved_or_posted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
